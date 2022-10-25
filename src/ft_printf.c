83ea65d4d9d8556bb6f72ffb03799dc4036b2c5c
MiguelCo01 <miguelmelocosta@gmail.com>
2022-10-25 12:38:37 +0100

    Corrected Norminette

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,12 +6,34 @@
 /*   By: mmelo-da <mmelo-da@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 11:18:06 by mmelo-da          #+#    #+#             */
-/*   Updated: 2022/10/25 12:14:14 by mmelo-da         ###   ########.fr       */
+/*   Updated: 2022/10/25 12:36:49 by mmelo-da         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "stdarg.h"
+
+
+int	ft_formats(char *format, va_list valist)
+{
+	if (*format == 'c')
+		return (ft_print_char((char ) va_arg(valist, int)));
+	if (*format == 's')
+		return (ft_print_string((char *) va_arg(valist, char *)));
+	if (*format == 'p')
+		return (ft_print_ptr((uintptr_t) va_arg(valist, uintptr_t)));
+	if (*format == 'i' || *format == 'd')
+		return (ft_print_int(va_arg(valist, int)));
+	if (*format == 'u')
+		return (ft_print_int(va_arg(valist, unsigned int)));
+	if (*format == 'x')
+		return (ft_print_hex(va_arg(valist, unsigned int), 0));
+	if (*format == 'X')
+		return (ft_print_hex(va_arg(valist, unsigned int), 1));
+	if (*format == '%')
+		return (ft_print_char('%'));
+	return (0);
+}
 
 int	ft_printf(const char *msg, ...)
 {
@@ -25,22 +47,7 @@ int	ft_printf(const char *msg, ...)
 		if (*msg == '%')
 		{
 			msg++;
-			if (*msg == 'c')
-				len += ft_print_char((char ) va_arg(valist, int));
-			if (*msg == 's')
-				len += ft_print_string((char *) va_arg(valist, char *));
-			if (*msg == 'p')
-				len += ft_print_ptr((uintptr_t) va_arg(valist, uintptr_t));
-			if (*msg == 'i' || *msg == 'd')
-				len += ft_print_int(va_arg(valist, int));
-			if (*msg == 'u')
-				len += ft_print_int(va_arg(valist, unsigned int));
-			if (*msg == 'x')
-				len += ft_print_hex(va_arg(valist, unsigned int), 0);
-			if (*msg == 'X')
-				len += ft_print_hex(va_arg(valist, unsigned int), 1);
-			if (*msg == '%')
-				len += ft_print_char('%');
+			len += ft_formats((char *)msg, valist);
 			msg++;
 		}
 		else
